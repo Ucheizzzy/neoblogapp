33c63ebcdbfba20c755abd2027f1ab5b551e0988
Mr.Lion <uchechukwuobiakor@gmail.com>
2024-01-15 19:16:01 +0100

    running production build

--- src/app/blog/new/layout.tsx
+++ src/app/blog/new/layout.tsx
@@ -4,12 +4,6 @@ import React from 'react'
 import { Session } from 'next-auth'
 import '@uploadthing/react/styles.css'
 
-export default function Layout({
-  children,
-  session,
-}: {
-  children: React.ReactNode
-  session: Session | null
-}) {
+export default function Layout({ children, session }: any) {
   return <SessionProvider session={session}>{children}</SessionProvider>
 }
