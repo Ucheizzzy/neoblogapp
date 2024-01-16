73594563168be23a428f9d4a0c8b66d075cea710
Mr.Lion <uchechukwuobiakor@gmail.com>
2024-01-16 15:36:57 +0100

    force dynamic routes

--- src/app/blog/all/page.tsx
+++ src/app/blog/all/page.tsx
@@ -1,7 +1,7 @@
 import prisma from '@/utils/db'
 import AllPosts from './AllPosts'
 import { Prisma } from '@prisma/client'
-
+export const dynamic = 'force-dynamic'
 export default async function page() {
   type PostWithCategories = Prisma.PostGetPayload<{
     include: { categories: true }
